4d392b246b804412f828237019e9d3355fca3b33
impossiblephrase <khaerudin.sendi@gmail.com>
2024-10-06 01:12:19 +0900

    deleted 2 var in starbackground

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -3,10 +3,8 @@
 import React ,{useState, useRef, Suspense} from 'react'
 import {Canvas, useFrame} from '@react-three/fiber'
 import {Points, PointMaterial, Preload} from '@react-three/drei'
-//@ts-expect-error
+// @ts-ignore
 import * as random from 'maath/random/dist/maath-random.esm'
-import { group } from 'console'
-import { div } from 'framer-motion/client'
 
 
 const StarBackground = (props: any) => {
